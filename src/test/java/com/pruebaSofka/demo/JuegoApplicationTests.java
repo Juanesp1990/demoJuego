49104bf5e5de07104b052299a17a254c2b6d6bc1
Juan Esteban Pineda <jpinedaangel@gmail.com>
2021-09-10 22:41:52 -0500

    creacion de proyecto

---
+++ src/test/java/com/pruebaSofka/demo/JuegoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebaSofka.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
